a7720743d14ce5bd7ebe25edd017fc9626a9f7e8
rxR4GE <heineke.pe@gmail.com>
2023-09-13 15:55:41 +0200

    Tests...

---
+++ 01/client.py
@@ -0,0 +1,14 @@
+import socket
+
+
+
+HOST = "localhost"
+PORT = 5050
+ADDR = (HOST, PORT)
+
+HEADER = 64
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect()
---
+++ 01/server.py
@@ -0,0 +1,46 @@
+import socket
+import threading
+
+
+HOST = "localhost"
+PORT = 5050
+ADDR = (HOST, PORT)
+
+HEADER = 64
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+
+    while connected:
+        message_len = int(conn.recv(HEADER_usertag).decode(FORMAT))
+        message = conn.recv(message_len).decode(FORMAT)
+        
+        if message == "!DISCONNECT":
+            connected = False
+
+        print(f"[{addr}] {message}")
+
+
+
+def start():
+    server.listen()
+
+    print(f"[LISTENING] Server is listening on {HOST}")
+
+    while True:
+        conn, addr = server.accept()
+
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+print("[STARTING] Server is starting...")
+start()
